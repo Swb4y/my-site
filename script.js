899e15e875675813482af55518d70f2326ab5556
Swb4y <dfrguiii45@hotmail.com>
2025-05-18 23:53:59 +0300

    Restore initial dynamic project from bunu folder

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form')?.addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('contact-form').style.display = 'none';
-  document.getElementById('success-message').style.display = 'block';
-});
